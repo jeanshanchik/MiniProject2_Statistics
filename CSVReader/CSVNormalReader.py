ba802c2f7a7ddb9f62d3b403a831a7843fb6fc83
Shannon <sp358@njit.edu>
2019-11-19 22:19:05 -0500

    Fix: updated Normal CSV reader to read data as a list

--- CSVReader/CSVNormalReader.py
+++ CSVReader/CSVNormalReader.py
@@ -12,7 +12,5 @@ class CsvNormalReader:
         with open(absolute_path(filepath), encoding='utf-8-sig') as list_data:
             csv_data = csv.reader(list_data, delimiter=',')
             for row in csv_data:
-                self.data = [num for elem in self.data for num in elem]
-            #self.data = [float(row) for row in self.data]
-            #self.data.append(row)
-                print(row)
+                self.data.append(row)
+            self.data = [num for elem in self.data for num in elem]
