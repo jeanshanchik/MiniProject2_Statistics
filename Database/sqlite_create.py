90ab4296fdefb5b67f07ddcfd06587a6672b4fd3
Kelley Shann <kelleyshann@gmail.com>
2019-11-29 13:13:25 -0500

    feature: added 8 item objects to table fix: updated item table toa dd quantity

--- Database/sqlite_create.py
+++ Database/sqlite_create.py
@@ -30,10 +30,8 @@ class Item(Base):
     name = Column(String(200), nullable=False)
     cost_price = Column(Numeric(10, 2), nullable=False)
     selling_price = Column(Numeric(10, 2), nullable=False)
-
-
-#     orders = relationship("Order", backref='customer')
-
+    quantity = Column(SmallInteger(), nullable=False)
+    # orders = relationship("Order", backref='customer')
 
 class Order(Base):
     __tablename__ = 'orders'
@@ -58,6 +56,7 @@ Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
+# Adding customers
 c1 = Customer(first_name='Toby',
               last_name='Miller',
               username='tmiller',
@@ -74,22 +73,19 @@ c2 = Customer(first_name='Scott',
               town='Beckinsdale'
               )
 
+c3 = Customer(first_name="John",
+              last_name="Lara",
+              username="johnlara",
+              email="johnlara@mail.com",
+              address="3073 Derek Drive",
+              town="Norfolk")
 
-c3 = Customer(
-first_name="John",
-    last_name="Lara",
-    username="johnlara",
-    email="johnlara@mail.com",
-    address="3073 Derek Drive",
-    town="Norfolk")
-
-c4 = Customer(
-    first_name="Sarah",
-    last_name="Tomlin",
-    username="sarahtomlin",
-    email="sarahtomlin@mail.com",
-    address="3572 Poplar Avenue",
-    town="Norfolk")
+c4 = Customer(first_name="Sarah",
+              last_name="Tomlin",
+              username="sarahtomlin",
+              email="sarahtomlin@mail.com",
+              address="3572 Poplar Avenue",
+              town="Norfolk")
 
 c5 = Customer(first_name='Toby',
               last_name='Miller',
@@ -106,4 +102,17 @@ c6 = Customer(first_name='Scott',
               town='Beckinsdale')
 
 session.add_all([c1, c2, c3, c4, c5, c6])
+session.commit()
+
+# Adding items
+i1 = Item(name='Chair', cost_price=9.21, selling_price=10.81, quantity=5)
+i2 = Item(name='Pen', cost_price=3.45, selling_price=4.51, quantity=3)
+i3 = Item(name='Headphone', cost_price=15.52, selling_price=16.81, quantity=50)
+i4 = Item(name='Travel Bag', cost_price=20.1, selling_price=24.21, quantity=50)
+i5 = Item(name='Keyboard', cost_price=20.1, selling_price=22.11, quantity=50)
+i6 = Item(name='Monitor', cost_price=200.14, selling_price=212.89, quantity=50)
+i7 = Item(name='Watch', cost_price=100.58, selling_price=104.41, quantity=50)
+i8 = Item(name='Water Bottle', cost_price=20.89, selling_price=25, quantity=50)
+
+session.add_all([i1, i2, i3, i4, i5, i6, i7, i8])
 session.commit()
