6beb3cab8db15977c6d93a9ed708a01992c57ea4
Kelley Shann <kelleyshann@gmail.com>
2019-11-29 13:04:44 -0500

    feature: added 4 more customer objects/data

--- Database/sqlite_create.py
+++ Database/sqlite_create.py
@@ -74,5 +74,36 @@ c2 = Customer(first_name='Scott',
               town='Beckinsdale'
               )
 
-session.add_all([c1, c2])
+
+c3 = Customer(
+first_name="John",
+    last_name="Lara",
+    username="johnlara",
+    email="johnlara@mail.com",
+    address="3073 Derek Drive",
+    town="Norfolk")
+
+c4 = Customer(
+    first_name="Sarah",
+    last_name="Tomlin",
+    username="sarahtomlin",
+    email="sarahtomlin@mail.com",
+    address="3572 Poplar Avenue",
+    town="Norfolk")
+
+c5 = Customer(first_name='Toby',
+              last_name='Miller',
+              username='tmiller',
+              email='tmiller@example.com',
+              address='1662 Kinney Street',
+              town='Wolfden')
+
+c6 = Customer(first_name='Scott',
+              last_name='Harvey',
+              username='scottharvey',
+              email='scottharvey@example.com',
+              address='424 Patterson Street',
+              town='Beckinsdale')
+
+session.add_all([c1, c2, c3, c4, c5, c6])
 session.commit()
